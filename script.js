78c5a9f95ffceaf7b0e3b16318a35f8d9ac62d76
Aiman Farhan <aimanfaruk98@gmail.com>
2021-11-08 21:56:15 +0800

    added isGameover

--- script.js
+++ script.js
@@ -24,4 +24,20 @@ function playRound(playerSelection,computerSelection) {
     }
 }
 
+function getRandomChoice(){
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch(randomNumber) {
+        case 0:
+            return "ROCK";
+        case 1:
+            return "PAPER";
+        case 2:
+            return "SCISSORS";
+    }
+}
+
+
+function isGameOver () {
+    return playerScore === 5 || computerScore === 5
+}
 
